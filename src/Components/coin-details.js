e3721cb795f8fb722a5fd8b7273be3be0e35b8d9
Jola Wroniecka <jola.wroniecka@pragmaticcoders.com>
2018-11-30 14:58:05 +0100

    set destructuring for coin details

--- src/Components/coin-details.js
+++ src/Components/coin-details.js
@@ -12,29 +12,19 @@ const ListItem = styled.div`
 `;
 
 
-const CoinDetails = ({coin: {
-  id,
-  symbol,
-  name,
-  website_slug,
-  quotes: {
-    USD: {
-        price,
-        volume_24h,
-        percent_change_1h,
-        percent_change_24h
-    }
-  }
-}}) =>
-  <List>
-    <ListItem>id: {id}</ListItem>
-    <ListItem>symbol: {symbol}</ListItem>
-    <ListItem>name: {name}</ListItem>
-    <ListItem>website slug: {website_slug}</ListItem>
-    <ListItem>price: {price}$</ListItem>
-    <ListItem>volume: {volume_24h}</ListItem>
-    <ListItem>percent change 1h: {percent_change_1h}%</ListItem>
-    <ListItem>percent change 24h: {percent_change_24h}%</ListItem>
-  </List>;
+const CoinDetails = ({coin}) => {
+  // const {name, symbol, quotes} = coin;
+
+  return (
+    <List>
+      <ListItem>{coin.symbol}</ListItem>
+      <ListItem>{coin.name}</ListItem>
+      {/*<ListItem>price: {coin.quotes.USD.price}$</ListItem>*/}
+      {/*<ListItem>percent change 7d: {coin.quotes.USD.percent_change_7d}%</ListItem>*/}
+      {/*<ListItem>percent change 24h: {coin.quotes.USD.percent_change_24h}%</ListItem>*/}
+    </List>
+  );
+};
 
 export default CoinDetails;
+
