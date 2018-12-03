7b7a5b9586ecbd3335aa0b07f87ae84efeb4b873
Jola Wroniecka <jola.wroniecka@pragmaticcoders.com>
2018-12-03 13:12:29 +0100

    delete useles variable, add regExp

--- src/app-data/data-processing.js
+++ src/app-data/data-processing.js
@@ -1,6 +1,6 @@
 import fetch from "isomorphic-fetch";
 
-export const fetchAppData = async ({ address, errorMessage }) => {
+export const fetchAppData = async ({ address }) => {
   console.log("address", address);
   try {
     const marketCapTicker = await fetch(address);
@@ -10,13 +10,15 @@ export const fetchAppData = async ({ address, errorMessage }) => {
     }
     return json;
   } catch (e) {
-    throw new Error(errorMessage + e);
+    throw new Error(e.message);
   }
 };
 
 export const findListingCoin = (coinToListing, coin) => {
+  const coinReg = new RegExp(coin, "ig");
+  console.log("coinReg", coinReg);
   const foundCoins = Object.values(coinToListing.data).find(item =>
-    [item.symbol, item.symbol.toLowerCase, item.name].includes(coin)
+    [item.symbol, item.symbol.toLowerCase(), item.name].includes(coin)
   );
   return foundCoins ? foundCoins : { message: "No coins found" };
 };
